3efa762711211bc88075715c6c045d05d0680f47
Max Heaton <dh64784@gmail.com>
2022-03-15 19:05:12 +0000

    Old archived thread support, better revival tech
    - Revive threads which were archived whilst the bot was offline;
    - Revive threads using the appropriate API method, not by sending a
      ghost message.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,100 @@
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::model::channel::GuildChannel;
+use serenity::http::Http;
+use serenity::model::channel::{ChannelType, GuildChannel};
+use serenity::model::guild::Guild;
 
+use std::collections::HashMap;
 use std::env;
+
+struct ChannelError {
+    public: bool,
+    channel: String,
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn thread_update(&self, context: Context, thread: GuildChannel) {
-        if let Some(metadata) = thread.thread_metadata {
-            if metadata.archived {
-                let result = thread
-                    .id
-                    .say(context.http.clone(), "Reviving archived channel...")
-                    .await;
-                if let Ok(msg) = result {
-                    if let Err(error) = thread.id.delete_message(context.http.clone(), msg.id).await
-                    {
-                        eprintln!("Failed to delete revival message: {}", error);
+    // Revive any threads which were archived whilst the bot was not listening.
+    async fn guild_create(&self, context: Context, guild: Guild, _is_new: bool) {
+        let mut channel_errors: HashMap<String, Vec<ChannelError>> = HashMap::new();
+        for (channel_id, channel) in guild.channels {
+            if channel.kind == ChannelType::Text {
+                match channel_id
+                    .get_archived_private_threads(context.http.clone(), None, None)
+                    .await
+                {
+                    Ok(threads_data) => {
+                        for thread in threads_data.threads {
+                            revive_thread(context.http.clone(), thread).await;
+                        }
                     }
-                } else {
-                    eprintln!("Failed to send revival message: {}", result.unwrap_err());
+                    Err(error) => {
+                        let vector = channel_errors.entry(error.to_string()).or_insert(vec![]);
+                        vector.push(ChannelError {
+                            public: false,
+                            channel: channel.name.clone(),
+                        });
+                    }
+                };
+                match channel_id
+                    .get_archived_public_threads(context.http.clone(), None, None)
+                    .await
+                {
+                    Ok(threads_data) => {
+                        for thread in threads_data.threads {
+                            revive_thread(context.http.clone(), thread).await;
+                        }
+                    }
+                    Err(error) => {
+                        let vector = channel_errors.entry(error.to_string()).or_insert(vec![]);
+                        vector.push(ChannelError {
+                            public: true,
+                            channel: channel.name,
+                        });
+                    }
+                };
+            }
+        }
+        // print any errors we encountered in a reasonably nicely formatted way, to help with diagnosing either actual code issues or Discord permission issues.
+        if channel_errors.len() > 0 {
+            eprintln!("Errors retrieving threads in guild {}", guild.name);
+            for error in channel_errors.keys() {
+                eprintln!("\t{}:", error);
+                for channel_error in channel_errors.get(error).unwrap() {
+                    eprintln!(
+                        "\t\t{}\t{}",
+                        if channel_error.public {
+                            "public"
+                        } else {
+                            "private"
+                        },
+                        channel_error.channel
+                    );
                 }
+            }
+        }
+    }
+
+    async fn thread_update(&self, context: Context, thread: GuildChannel) {
+        revive_thread(context.http, thread).await;
+    }
+}
+
+async fn revive_thread(http: impl AsRef<Http>, thread: GuildChannel) {
+    if let Some(metadata) = thread.thread_metadata {
+        if metadata.archived {
+            let result = thread
+                .id
+                .edit_thread(http, |thread| thread.archived(false))
+                .await;
+            match result {
+                Ok(_) => (),
+                Err(error) => eprintln!(
+                    "Failed to revive thread (does the bot have permission?): {}",
+                    error
+                ),
             }
         }
     }
